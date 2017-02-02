454e898013ff46ed317c3dd8a59ff9a85457ec10
Alex Kavanagh <alex@ajkavanagh.co.uk>
2017-02-02 11:09:55 +0000

    Initial Commit

---
+++ reactive/layer_flask_master.py
@@ -0,0 +1,24 @@
+from charms.reactive import when, when_not, set_state
+import charmhelpers.core.hookenv as hookenv
+
+
+@when_not('layer-flask-master.installed')
+def install_layer_flask_master():
+    # Do your setup here.
+    #
+    # If your charm has other dependencies before it can install,
+    # add those as @when() clauses above., or as additional @when()
+    # decorated handlers below
+    #
+    # See the following for information about reactive charms:
+    #
+    #  * https://jujucharms.com/docs/devel/developer-getting-started
+    #  * https://github.com/juju-solutions/layer-basic#overview
+    #
+    set_state('layer-flask-master.installed')
+
+
+@when('layer-flask-master.installed')
+@when('flask-master.related')
+def send_message(slave):
+    slave.send_message(hookenv.config('motd'))
